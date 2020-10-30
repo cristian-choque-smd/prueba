a7c33430354f133f61bad89204432a2b49140072
Cristianchl <cristian.ramiro.chl@gmail.com>
2020-10-30 08:41:44 -0400

    agregando gauge v2

---
+++ specs/CreatePatient.spec
@@ -0,0 +1,14 @@
+# Create Patient Sharemedata
+
+This is an executable specification file. This file follows markdown syntax.
+Every heading in this file denotes a scenario. Every bulleted point denotes a step.
+
+
+## Create sharemedata account
+
+* Open page
+* Login with patient account
+* PopUp add patient
+* Create new patient
+* Exit from account
+
---
+++ tests/step_implementation_1.js
@@ -0,0 +1,35 @@
+/* globals gauge*/
+"use strict";
+const { openBrowser, write, closeBrowser, goto, press, screenshot, text, focus, textBox, toRightOf, click, $, into, dropDown} = require('taiko');
+
+const assert = require("assert");
+const path = require("path");
+const headless = process.env.headless_chrome.toLowerCase() === 'false';
+
+
+beforeSuite(async () => {
+	await openBrowser({ headless: headless });
+	//await openBrowser({ headless: headless });
+});
+
+afterSuite(async () => {
+    await closeBrowser();
+});
+
+step("Search by patient", async function() {
+    await click($("#header-option-patients"));
+
+    await write("zcristian",into(textBox({placeholder:"Nombre"})));
+    
+    await click($("#patient-list-see"));
+    
+    
+});
+
+step("Delete patient", async function() {
+    
+	await click("Editar información",{ navigationTimeout: 3000});
+    await click("Eliminar");
+    //await click("Eliminar");
+    //goto("url",{timeout:60000})
+});
