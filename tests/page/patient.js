afc14ab09f5ab0c748aed1124211037676ffebb1
Cristianchl <cristian.ramiro.chl@gmail.com>
2020-11-13 21:47:21 -0400

    prueba merge branch

---
+++ tests/page/patient.js
@@ -0,0 +1,11 @@
+const {write, click, inputfield, $, into, textBox} = require("taiko");
+
+class LoginPage {
+    async funcion1 (username, password) {
+        await write(username, into(textBox({id: "email"})));
+        await write(password, into(textBox({id: "password"})));
+        //await click($("//button[@id='login-button']"));
+        await click($("#login-button"));
+    }
+}
+module.exports = new LoginPage();
